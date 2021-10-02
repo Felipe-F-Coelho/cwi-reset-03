03a1a04532cae65732b08250db2c0747c31d9a96
Felipe Coelho <coelho.felipe.rj@gmail.com>
2021-10-02 17:49:49 -0300

    Adicionando desafio aula 01

--- Aula 01/Exercícios/projeto-1/src/main/java/br/com/cwi/reset/projeto1/Exercicios1.java
+++ Aula 01/Exercícios/projeto-1/src/main/java/br/com/cwi/reset/projeto1/Exercicios1.java
@@ -1,28 +1,57 @@
 package br.com.cwi.reset.projeto1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Exercicios1 {
 
     public Integer somarLista(List<Integer> numeros) {
-        return 10;
+
+        int soma = 0;
+
+        for (int i = 0; i < numeros.size(); i++) {
+            soma += numeros.get(i);
+        }
+
+        return soma;
     }
 
     public Double calcularMedia(List<Integer> numeros) {
-        return 10.0;
+        double soma = 0;
+
+        for (int i = 0; i < numeros.size(); i++) {
+            soma += numeros.get(i);
+        }
+        return soma / numeros.size();
     }
 
     public Integer obterMaiorNumero(List<Integer> numeros) {
-        return 10;
+        int numeroMaior = 0;
+        for(int i = 0; i < numeros.size(); i++){
+            if(numeros.get(i) > numeroMaior){
+                numeroMaior = numeros.get(i);
+            }
+        }
+        return numeroMaior;
     }
 
     public String obterPalavraInvertida(String palavra) {
-        return "edcba";
+        String palavraInvertida = "";
+
+        for (int i = palavra.length() - 1; i >= 0; i--){
+            palavraInvertida += palavra.charAt(i);
+        }
+
+        return  palavraInvertida;
     }
 
     public List<Integer> ordenarLista(List<Integer> numeros) {
-        return Arrays.asList(1, 2, 3, 4, 5);
+        List<int> ordenacão = new ArrayList<>();
+
+        
+
+        return ordenacão;
     }
 }
 
